b00dcf693661222b14732d194be6eb634017b346
Alexandra Rusak <alexandra.myagkova@gmail.com>
2024-04-06 20:04:45 +0300

    added active link highlight in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {LoginService} from "../services/login.service";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, NgIf],
+  imports: [RouterLink, NgIf, RouterLinkActive],
   // template: `<!--<p>test navbar template</p>-->`,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
@@ -15,5 +15,14 @@ export class NavbarComponent {
 
   constructor(public loginService: LoginService) {
   }
+
+  // handleClick($event:any) {
+  //   console.log($event.target)
+  //   if (!$event.target.classList.contains("nav-link")) {
+  //     console.log("not my target")
+  //     return
+  //   }
+  //   $event.target.classList.toggle("active")
+  // }
 
 }
